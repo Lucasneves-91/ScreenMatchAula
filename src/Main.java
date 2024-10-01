7ed11b643fb3d783b54cead3c0ecd1f259b48407
Lucas Neves <lucas.ccdr@gmail.com>
2024-10-01 17:32:06 -0300

    Second block of class included.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
+import br.com.screenMatch.models.Film;
+
 public class Main {
     public static void main(String[] args) {
         Film film = new Film();
+
+        film.setName("The Matrix");
+        film.setYear(1999);
+        film.setDuration(135);
+        film.setPlan(true);
+
+        film.description();
+        film.avaliation(9);
+        film.avaliation(8);
+        film.avaliation(8);
+
+        System.out.println("Média de avaliações do filme: " + film.media());
 
 
     }
---
+++ src/br/com/screenMatch/models/Film.java
@@ -0,0 +1,75 @@
+package br.com.screenMatch.models;
+
+public class Film {
+    private String name;
+    private int year;
+    private int duration;
+    private boolean plan;
+    private double sumAvaliation;
+    private int totalAvaliation;
+
+    public void description() {
+        System.out.println("Nome do filme: " + name);
+        System.out.println("Ano de lançamento: " + year);
+        System.out.println("Duração em minutos: " + duration);
+        System.out.println("Incluído no plano: + " + plan);
+
+    }
+
+    public void avaliation(double note) {
+        sumAvaliation += note;
+        totalAvaliation ++;
+    }
+
+    public double media(){
+        return sumAvaliation / totalAvaliation;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public boolean isPlan() {
+        return plan;
+    }
+
+    public void setPlan(boolean plan) {
+        this.plan = plan;
+    }
+
+    public double getSumAvaliation() {
+        return sumAvaliation;
+    }
+
+    public void setSumAvaliation(double sumAvaliation) {
+        this.sumAvaliation = sumAvaliation;
+    }
+
+    public int getTotalAvaliation() {
+        return totalAvaliation;
+    }
+
+    public void setTotalAvaliation(int totalAvaliation) {
+        this.totalAvaliation = totalAvaliation;
+    }
+}
